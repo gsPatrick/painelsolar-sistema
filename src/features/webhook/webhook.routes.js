7d3398fc474f402d9e47215cf2f63aebc3dab76e
Patrick Siqueira <patrickgsiqueira@hotmail.com>
2026-01-06 18:01:38 -0300

    subindo mudancas

--- src/features/webhook/webhook.routes.js
+++ src/features/webhook/webhook.routes.js
@@ -4,10 +4,10 @@ const webhookController = require('./webhook.controller');
 const router = express.Router();
 
 // Z-API webhook (no auth - external service)
-router.post('/z-api', webhookController.handleZApiWebhook);
+router.post('/z-api', (req, res) => webhookController.handleZApiWebhook(req, res));
 
 // Meta (Facebook) webhooks
-router.get('/meta', webhookController.verifyMetaWebhook);
-router.post('/meta', webhookController.handleMetaWebhook);
+router.get('/meta', (req, res) => webhookController.verifyMetaWebhook(req, res));
+router.post('/meta', (req, res) => webhookController.handleMetaWebhook(req, res));
 
 module.exports = router;
