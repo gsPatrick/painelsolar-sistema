c813fa2e9f2e89d8337abcced45a268a29bee986
Patrick Siqueira <patrickgsiqueira@hotmail.com>
2026-01-06 17:51:04 -0300

    subindo mudancas

--- src/features/message/message.routes.js
+++ src/features/message/message.routes.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const messageController = require('./message.controller');
-const authMiddleware = require('../auth/auth.middleware');
+const { authenticate } = require('../auth/auth.middleware');
 
 // Protect routes
-router.use(authMiddleware);
+router.use(authenticate);
 
 router.get('/history/:leadId', messageController.getHistory);
 router.post('/', messageController.create);
